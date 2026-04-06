e1b4d78681508144b4fd7f06a3c07bc4f807256c
gatunyus <gatunyus@gmail.com>
2026-04-06 16:14:52 +0700

    Update code.ino

--- Lab/Lab03 Obstacle Avoidance Sensor/code.ino
+++ Lab/Lab03 Obstacle Avoidance Sensor/code.ino
@@ -8,9 +8,9 @@ void setup() {
 void loop() {
   int val = digitalRead(irPin);
   if ( val == 0 ) {
-    Serial.println("มืด");
+    Serial.println("มีสิ่งกีดขวาง");
   } else {
-    Serial.println("สว่าง");
+    Serial.println("ไม่มีสิ่งกีดขวาง");
   }
   delay(100);
 }
