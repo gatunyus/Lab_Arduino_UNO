6e6a606be958cbfbcd8e708df7456928cdd450cb
gatunyus <gatunyus@gmail.com>
2026-04-06 16:29:42 +0700

    Update code.ino

--- Lab/Lab05 Water Level Sensor/code.ino
+++ Lab/Lab05 Water Level Sensor/code.ino
@@ -6,24 +6,16 @@ void setup() {
 
 void loop() {
   int val = analogRead(waterPin);
-  Serial.print("วัดค่าได้ : ");
-  Serial.print(val);
-  Serial.print(" - สถานะ: ");
-
   if (val < 10) {
-    // ถ้าน้อยกว่า 10 ถือว่าไม่มีน้ำเลย
     Serial.println("แห้ง");
   } 
   else if (val >= 10 && val <= 300) {
-    // ค่าระหว่าง 10 ถึง 300
     Serial.println("ชื้นเล็กน้อย");
   } 
   else if (val > 300 && val <= 600) {
-    // ค่าระหว่าง 301 ถึง 600
     Serial.println("ชื้นมาก");
   } 
   else {
-    // ค่าที่มากกว่า 600 ขึ้นไป
     Serial.println("เปียก");
   }
 
